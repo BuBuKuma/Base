2047153c2a8480820b491bcbfdecbb0a8c536779
BuBuKuma <31237269+BuBuKuma@users.noreply.github.com>
2020-03-16 14:24:46 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 alert('123')
+var MYLIBRARY = MYLIBRARY || (function(){
+    var _args = {}; // private
+
+    return {
+        init : function(Args) {
+            _args = Args;
+            // some other initialising
+        },
+        helloWorld : function() {
+            alert('Hello World! -' + _args[0]);
+        }
+    };
+}());
